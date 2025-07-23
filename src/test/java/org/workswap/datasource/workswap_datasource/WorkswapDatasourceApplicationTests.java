32e35b6341885ceaed77b155fb60902e7ee0d111
Kirya <kkodolov40@gmail.com>
2025-07-23 21:48:39 +0300

    first commit

---
+++ src/test/java/org/workswap/datasource/workswap_datasource/WorkswapDatasourceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.workswap.datasource.workswap_datasource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkswapDatasourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
